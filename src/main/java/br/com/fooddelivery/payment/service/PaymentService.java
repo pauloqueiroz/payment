7f6611af4cf3a17aa7e1edd6d39e0747f1c6739c
Paulo Sérgio <pauloqueiroz16@gmail.com>
2022-12-14 00:32:52 -0300

    Adding service class with CRUD operations

---
+++ src/main/java/br/com/fooddelivery/payment/service/PaymentService.java
@@ -0,0 +1,55 @@
+package br.com.fooddelivery.payment.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.com.fooddelivery.payment.dto.PaymentDto;
+import br.com.fooddelivery.payment.model.Payment;
+import br.com.fooddelivery.payment.repository.PaymentRepository;
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class PaymentService {
+	
+	private PaymentRepository repository;
+
+	private ModelMapper mapper;
+
+	@Autowired
+	public PaymentService(PaymentRepository repository, ModelMapper modelMapper) {
+		super();
+		this.repository = repository;
+		this.mapper = modelMapper;
+	}
+	
+	public PaymentDto getById(Long id) {
+		Payment payment = repository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+		return mapper.map(payment, PaymentDto.class);
+	}
+	
+	
+	public Page<PaymentDto> getAll(Pageable pageable){
+		return repository.findAll(pageable).map(payemnt-> mapper.map(payemnt, PaymentDto.class));
+	}
+	
+	public PaymentDto create(PaymentDto paymentDto) {
+		Payment payment = mapper.map(paymentDto, Payment.class);
+		Payment paymentSaved = repository.save(payment);
+		return mapper.map(paymentSaved, PaymentDto.class);
+		
+	}
+	
+	public PaymentDto update(Long id, PaymentDto paymentDto) {
+		Payment payment = mapper.map(paymentDto, Payment.class);
+		payment.setId(id);
+		Payment updatedPayment = repository.save(payment);
+		return mapper.map(updatedPayment, PaymentDto.class);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+}
